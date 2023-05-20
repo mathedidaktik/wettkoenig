08b0da95a3e68e91a3741ba95417b8f2a7f2ff8b
Julia Ritz <julia.ritz-s87@ruhr-uni-bochum.de>
2023-05-20 16:21:47 +0200

    Added more colors

--- components/Dice.vue
+++ components/Dice.vue
@@ -4,8 +4,8 @@
         <button @click="changeDiceMode" class="button text-white rounded-md px-4 py-2" v-if="useDiyDice">Nutze fertige Würfel</button>
 
         <div class="flex flex-col items-start">
-            <label for="num-rolls" class="text-lg font-medium">Anzahl würfe:</label>
-            <input type="number" id="num-rolls" class="border-gray-300 border rounded-md px-3 py-2 w-full" min="0" max="1000000" v-bind:keyup=enforceMinMax(this) v-model="numberOfRolls">
+            <label for="num-rolls" class="text-lg font-medium">Anzahl Würfe:</label>
+            <input type="number" id="num-rolls" class="border-gray-300 border rounded-md px-3 py-2 w-full" min="1" max="1000000" v-bind:keyup=enforceMinMax() v-model="numberOfRolls">
         </div>
 
         <select ref="diceSelect" id="diceChooser" :onChange="setDiceSides" class="border-gray-300 border rounded-md px-3 py-2 w-full" v-if="!useDiyDice">
@@ -20,7 +20,7 @@
             </div>
             <div class="flex flex-col items-start" v-if="useDiyDice">
                 <label for="red-sides" class="text-lg font-medium redInput w-full block">Rote Seiten:</label>
-                <input type="number" id="red-sides" class="border-gray-300 border rounded-md px-3 py-2 w-full block"  min="0" max="1000" v-bind:keyup=enforceMinMax(this) v-model="redSides">
+                <input type="number" id="red-sides" class="border-gray-300 border rounded-md px-3 py-2 w-full block"  min="0" max="1000" v-bind:keyup=enforceMinMax() v-model="redSides">
             </div>
 
             <div v-if="!useDiyDice">
@@ -28,7 +28,7 @@
             </div>
             <div class="flex flex-col items-start" v-if="useDiyDice">
                 <label for="yellow-sides" class="text-lg font-medium yellowInput w-full block">Gelbe Seiten:</label>
-                <input type="number" id="yellow-sides" class="border-gray-300 border rounded-md px-3 py-2 w-full block"  min="0" max="1000" v-bind:keyup=enforceMinMax(this) v-model="yellowSides">
+                <input type="number" id="yellow-sides" class="border-gray-300 border rounded-md px-3 py-2 w-full block"  min="0" max="1000" v-bind:keyup=enforceMinMax() v-model="yellowSides">
             </div>
 
             <div v-if="!useDiyDice">
@@ -36,9 +36,36 @@
             </div>
             <div class="flex flex-col items-start" v-if="useDiyDice">
                 <label for="blue-sides" class="text-lg font-medium blueInput w-full block">Blaue Seiten:</label>
-                <input type="number" id="blue-sides" class="border-gray-300 border rounded-md px-3 py-2 w-full block"  min="0" max="1000" v-bind:keyup=enforceMinMax(this) v-model="blueSides">
+                <input type="number" id="blue-sides" class="border-gray-300 border rounded-md px-3 py-2 w-full block"  min="0" max="1000" v-bind:keyup=enforceMinMax() v-model="blueSides">
+            </div>
+
+            <div class="flex flex-col items-start" v-if="useDiyDice">
+                <label for="lightblue-sides" class="text-lg font-medium lightblueInput w-full block">Hellblaue Seiten:</label>
+                <input type="number" id="lightblue-sides" class="border-gray-300 border rounded-md px-3 py-2 w-full block"  min="0" max="1000" v-bind:keyup=enforceMinMax() v-model="lightblueSides">
+            </div>
+
+            <div class="flex flex-col items-start" v-if="useDiyDice">
+                <label for="green-sides" class="text-lg font-medium greenInput w-full block">Grüne Seiten:</label>
+                <input type="number" id="green-sides" class="border-gray-300 border rounded-md px-3 py-2 w-full block"  min="0" max="1000" v-bind:keyup=enforceMinMax() v-model="greenSides">
+            </div>
+
+            <div class="flex flex-col items-start" v-if="useDiyDice">
+                <label for="orange-sides" class="text-lg font-medium orangeInput w-full block">Orange Seiten:</label>
+                <input type="number" id="orange-sides" class="border-gray-300 border rounded-md px-3 py-2 w-full block"  min="0" max="1000" v-bind:keyup=enforceMinMax() v-model="orangeSides">
+            </div>
+
+            <div class="flex flex-col items-start" v-if="useDiyDice">
+                <label for="pink-sides" class="text-lg font-medium pinkInput w-full block">Pink Seiten:</label>
+                <input type="number" id="pink-sides" class="border-gray-300 border rounded-md px-3 py-2 w-full block"  min="0" max="1000" v-bind:keyup=enforceMinMax() v-model="pinkSides">
+            </div>
+
+            <div class="flex flex-col items-start" v-if="useDiyDice">
+                <label for="black-sides" class="text-lg font-medium blackInput w-full block">Schwarze Seiten:</label>
+                <input type="number" id="black-sides" class="border-gray-300 border rounded-md px-3 py-2 w-full block"  min="0" max="1000" v-bind:keyup=enforceMinMax() v-model="blackSides">
             </div>
         </div>
+        <lable v-if="warningRolls" class="text-red-700">Es Muss mindestens einen Wurf und maximal 1000000 Würfe geben.</lable>
+        <label v-if="warningSites" class="text-red-700">Die Seitenanzahl der einzelnen farben muss mindestens 0 und maximal 1000 betragen.</label>
     </div>
 </template>
 
@@ -50,11 +77,19 @@
     data() {
         return {
         numberOfRolls: 0,
+        totalSides: 0,
+        redSides: 0,
         yellowSides: 0,
         blueSides: 0,
-        redSides: 0,
+        lightblueSides: 0,
+        greenSides: 0,
+        orangeSides: 0,
+        pinkSides: 0,
+        blackSides: 0,
         rolls: [],
         useDiyDice: false,
+        warningRolls: false,
+        warningSites: false,
         selectedOption:''
         }
     },
@@ -65,49 +100,70 @@
       }
     },
     methods: {
-        enforceMinMax(el) {
-            console.log("befor if");
-            if (el.value != "") {
-                console.log("inside if");
-                console.log(el);
-                console.log("id: " + el.id)
-                console.log("value: " + el.value + "min: " + el.min + "max: " + el.max)
-                if (parseInt(el.value) < parseInt(el.min)) {
-                    console.log(el);
-                    el.value = el.min;
-                }
-                if (parseInt(el.value) > parseInt(el.max)) {
-                    console.log(el);
-                    el.value = el.max;
-                }
+        enforceMinMax() {
+            if (this.numberOfRolls < 0 | this.numberOfRolls > 1000000) {
+                this.warningRolls = true;
+            } 
+            else {
+                this.warningRolls = false;
+            }
+            if (this.yellowSides < 0 | this.yellowSides > 1000 | this.blueSides < 0 | this.blueSides > 1000 | this.redSides < 0 | this.redSides > 1000 |
+                this.lightblueSides < 0 | this.lightblueSides > 1000 | this.greenSides < 0 | this.greenSides > 1000 | this.orangeSides < 0 | this.orangeSides > 1000 |
+                this.pinkSides < 0 | this.pinkSides > 1000 | this.blackSides < 0 | this.blackSides > 1000) {
+                this.warningSites = true;
+            }
+            else {
+                this.warningSites = false;
             }
         },
         rollDice() {
             const rolls = [];
+            if (this.warningRolles |this.warningSites) {
+                this.rolls = rolls;
+            }
 
-            if ((this.redSides + this.yellowSides + this.blueSides) <= 0) {
+            this.totalSides = this.redSides + this.yellowSides + this.blueSides + this.lightblueSides + this.greenSides + this.orangeSides + this.pinkSides + this.blackSides;
+
+            if (this.totalSides <= 0) {
                 this.rolls = rolls;
                 return;
             }
 
             for (let i = 0; i < this.numberOfRolls; i++) {
-                const randomRoll = Math.floor(Math.random() * (this.yellowSides + this.blueSides + this.redSides)) + 1
+                const randomRoll = Math.floor(Math.random() * this.totalSides) + 1
                 if (randomRoll <= this.yellowSides) {
                     rolls.push('yellow')
                 } else if (randomRoll <= this.yellowSides + this.blueSides) {
                     rolls.push('blue')
-                } else {
+                } else if (randomRoll <= this.yellowSides + this.blueSides + this.redSides){
                     rolls.push('red')
-                }
+                } else if (randomRoll <= this.yellowSides + this.blueSides + this.redSides + this.lightblueSides) {
+                    rolls.push('lightblue')
+                } else if (randomRoll <= this.yellowSides + this.blueSides + this.redSides + this.lightblueSides + this.greenSides){
+                    rolls.push('green')
+                } else if (randomRoll <= this.yellowSides + this.blueSides + this.redSides + this.lightblueSides + this.greenSides + this.orangeSides) {
+                    rolls.push('orange')
+                } else if (randomRoll <= this.yellowSides + this.blueSides + this.redSides + this.lightblueSides + this.greenSides + this.orangeSides + this.pinkSides){
+                    rolls.push('pink')
+                } else {
+                    rolls.push('black')
+                } 
+                
             }
             this.rolls = rolls
         },
         changeDiceMode() {
             this.useDiyDice = !this.useDiyDice;
 
-            this.blueSides = 0;
-            this.yellowSides = 0;
             this.redSides = 0;
+            this.yellowSides = 0;
+            this.blueSides = 0;
+            
+            this.lightblueSides = 0;
+            this.greenSides = 0;
+            this.orangeSides = 0;
+            this.pinkSides = 0;
+            this.blackSides = 0;
 
             this.$emit('dice-changed', this.id);
 
--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,14 @@
             <button @click="secondDiceCheck" class="button text-white rounded-md px-4 p-4 m-0" v-if="!secondDice">Nutze zwei Würfel</button>
             <button @click="secondDiceCheck" class="button text-white rounded-md px-4 p-4 m-0" v-if="secondDice">Nutze einen Würfel</button>
             <div id="x-dices-container" class="flex flex-row space-x-4 mt-1">
-                <div class="y-dice-container flex flex-col grow">
+                <div class="y-dice-container flex flex-col grow lg:w-1/2 xl:w-1/2">
                     <Dice ref="dice1" :id="0" @dice-changed="setTableToZero" />
                     <div class="flex flex-col space-y-8">
                         <button type="button" @click="rollDice1" class="button text-white rounded-md px-4 py-2 mt-4">Roll Dice</button>    
                     </div>
                 </div>
                 
-                <div class="y-dice-container flex flex-col grow" v-if="secondDice">
+                <div class="y-dice-container flex flex-col grow lg:w-1/2 xl:w-1/2" v-if="secondDice">
                     <Dice ref="dice2" :id="1" @dice-changed="setTableToZero" />
                     <div class="flex flex-col space-y-8">
                         <button type="button" @click="rollDice2" class="button text-white rounded-md px-4 py-2 mt-4">Roll Dice</button>
@@ -44,23 +44,27 @@ export default{
                 secondDice: false,
                 chart: null,
                 chartData: {
-                    labels: ['Rot', 'Gelb', 'Blau'],
+                    labels: ['Rot', 'Gelb', 'Blau', 'Hellblau', 'Grün', 'Orange', 'Pink', 'Schwarz'],
                     datasets: [
                     {
                         //axis: 'y',
                         label: 'Würfel 1',
-                        backgroundColor: ['rgba(230, 51, 42, 0.2)', 'rgba(255, 204, 0, 0.2)', 'rgba(0,53,96, 0.2)'],
-                        borderColor: ['rgb(230, 51, 42)', 'rgb(255, 204, 0)', 'rgb(0,53,96)'],
+                        backgroundColor: ['rgba(230, 51, 42, 0.2)', 'rgba(255, 204, 0, 0.2)', 'rgba(0,53,96, 0.3)', 'rgba(86, 180, 233, 0.5)', 
+                        'rgba(0, 158, 115, 0.5)', 'rgba(230, 159, 0, 0.5)', 'rgba(204, 121, 167, 0.5)', 'rgba(0, 0, 0, 0.3)'],
+                        borderColor: ['rgb(230, 51, 42)', 'rgb(255, 204, 0)', 'rgb(0,53,96)', 'rgb(86, 180, 233)', 
+                        'rgb(0, 158, 115)', 'rgb(230, 159, 0)', 'rgb(204, 121, 167)', 'rgb(0, 0, 0)'],
                         borderWidth: 1,
-                        data: [0, 0, 0],
+                        data: [0, 0, 0, 0, 0, 0, 0, 0],
                     },
                     {
                         //axis: 'y',
                         label: 'Würfel 2',
-                        backgroundColor: ['rgba(230, 51, 42, 0.4)', 'rgba(255, 204, 0, 0.4)', 'rgba(0,53,96, 0.4)'],
-                        borderColor: ['rgb(230, 51, 42)', 'rgb(255, 204, 0)', 'rgb(0,53,96)'],
+                        backgroundColor: ['rgba(230, 51, 42, 0.2)', 'rgba(255, 204, 0, 0.2)', 'rgba(0,53,96, 0.3)', 'rgba(86, 180, 233, 0.5)', 
+                        'rgba(0, 158, 115, 0.5)', 'rgba(230, 159, 0, 0.5)', 'rgba(204, 121, 167, 0.5)', 'rgba(0, 0, 0, 0.3)'],
+                        borderColor: ['rgb(230, 51, 42)', 'rgb(255, 204, 0)', 'rgb(0,53,96)', 'rgb(86, 180, 233)', 
+                        'rgb(0, 158, 115)', 'rgb(230, 159, 0)', 'rgb(204, 121, 167)', 'rgb(0, 0, 0)'],
                         borderWidth: 1,
-                        data: [0, 0, 0],
+                        data: [0, 0, 0, 0, 0, 0, 0, 0],
                     },
                     ]     
                },
@@ -117,11 +121,11 @@ export default{
             },
             setTableToZero(diceId) {
                 if (diceId == 0) {
-                    this.myChart.data.datasets[0].data = [0, 0, 0];
+                    this.myChart.data.datasets[0].data = [0, 0, 0, 0, 0, 0, 0, 0];
                     this.myChart.update();
                 }
                 else if (diceId == 1) {
-                    this.myChart.data.datasets[1].data = [0, 0, 0];
+                    this.myChart.data.datasets[1].data = [0, 0, 0, 0, 0, 0, 0, 0];
                     this.myChart.update();
                 }
             },
@@ -130,27 +134,38 @@ export default{
                 var newData = [
                         ((rolls.filter(roll => roll === 'red').length) / numberOfRolls) * 100,
                         ((rolls.filter(roll => roll === 'yellow').length) / numberOfRolls) * 100,
-                        ((rolls.filter(roll => roll === 'blue').length) / numberOfRolls) * 100
+                        ((rolls.filter(roll => roll === 'blue').length) / numberOfRolls) * 100,
+                        ((rolls.filter(roll => roll === 'lightblue').length) / numberOfRolls) * 100,
+                        ((rolls.filter(roll => roll === 'green').length) / numberOfRolls) * 100,
+                        ((rolls.filter(roll => roll === 'orange').length) / numberOfRolls) * 100,
+                        ((rolls.filter(roll => roll === 'pink').length) / numberOfRolls) * 100,
+                        ((rolls.filter(roll => roll === 'black').length) / numberOfRolls) * 100,
                     ]
 
                 this.myChart.data.datasets[datasetIndex].data = newData;
                 
-                console.log("display chart");
                 this.myChart.update();
                     
             },
             rollDice1() {
                 let dice1 = this.$refs.dice1;
+                if (dice1.warningRolls | dice1.warningSites) {
+                    this.displayChart([], 0, 0);
+                    return;
+                }
                 dice1.rollDice();
                 this.displayChart(dice1.rolls, dice1.numberOfRolls, 0);
             },
 
             rollDice2(){
                 let dice2 = this.$refs.dice2;
+                if (dice2.warningRolls | dice2.warningSites) {
+                    this.displayChart([], 0, 1);
+                    return;
+                }
                 dice2.rollDice();
                 this.displayChart(dice2.rolls, dice2.numberOfRolls, 1);
             },
-            
             
             changeChart() {
                 this.myChart.options.indexAxis = this.myChart.options.indexAxis === 'x' ? 'y' : 'x';
@@ -159,7 +174,6 @@ export default{
                 this.myChart.options.scales.x = lodash.cloneDeep(this.myChart.options.scales.y);
                 this.myChart.options.scales.y = scalesX;
 
-                console.log("changechart");
                 this.myChart.update();
             },
         },
