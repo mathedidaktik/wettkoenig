3bc6e86598b42b18ca2112838eabbcd4569ea012
Julia Ritz <julia.ritz-s87@ruhr-uni-bochum.de>
2023-07-18 22:41:04 +0200

    Change the charts labels; Change the text displayed by the tooltips

--- pages/index.vue
+++ pages/index.vue
@@ -50,24 +50,26 @@ export default{
                     {
                         //axis: 'y',
                         label: 'Würfel 1',
-                        backgroundColor: ['rgba(230, 51, 42, 0.2)', 'rgba(255, 204, 0, 0.2)', 'rgba(0,53,96, 0.3)', 'rgba(86, 180, 233, 0.5)', 
+                        backgroundColor: ['rgba(213, 94, 0, 0.4)', 'rgba(240, 228, 66, 0.2)', 'rgba(0, 114, 178, 0.3)', 'rgba(86, 180, 233, 0.5)', 
                         'rgba(0, 158, 115, 0.5)', 'rgba(230, 159, 0, 0.5)', 'rgba(204, 121, 167, 0.5)', 'rgba(0, 0, 0, 0.3)'],
-                        borderColor: ['rgb(230, 51, 42)', 'rgb(255, 204, 0)', 'rgb(0,53,96)', 'rgb(86, 180, 233)', 
+                        borderColor: ['rgb(213, 94, 0)', 'rgb(240, 228, 66)', 'rgb(0,114,178)', 'rgb(86, 180, 233)', 
                         'rgb(0, 158, 115)', 'rgb(230, 159, 0)', 'rgb(204, 121, 167)', 'rgb(0, 0, 0)'],
                         borderWidth: 1,
                         data: [0, 0, 0, 0, 0, 0, 0, 0],
-                        rolls: [0, 0, 0, 0, 0, 0, 0, 0]
+                        rolls: [0, 0, 0, 0, 0, 0, 0, 0],
+                        labels: ['Rot', 'Gelb', 'Blau', 'Hellblau', 'Grün', 'Orange', 'Pink', 'Schwarz'],
                     },
                     {
                         //axis: 'y',
                         label: 'Würfel 2',
-                        backgroundColor: ['rgba(230, 51, 42, 0.2)', 'rgba(255, 204, 0, 0.2)', 'rgba(0,53,96, 0.3)', 'rgba(86, 180, 233, 0.5)', 
+                        backgroundColor: ['rgba(213, 94, 0, 0.4)', 'rgba(240, 228, 66, 0.2)', 'rgba(0, 114, 178, 0.3)', 'rgba(86, 180, 233, 0.5)', 
                         'rgba(0, 158, 115, 0.5)', 'rgba(230, 159, 0, 0.5)', 'rgba(204, 121, 167, 0.5)', 'rgba(0, 0, 0, 0.3)'],
-                        borderColor: ['rgb(230, 51, 42)', 'rgb(255, 204, 0)', 'rgb(0,53,96)', 'rgb(86, 180, 233)', 
+                        borderColor: ['rgb(213, 94, 0)', 'rgb(240, 228, 66)', 'rgb(0,114,178)', 'rgb(86, 180, 233)', 
                         'rgb(0, 158, 115)', 'rgb(230, 159, 0)', 'rgb(204, 121, 167)', 'rgb(0, 0, 0)'],
                         borderWidth: 1,
                         data: [0, 0, 0, 0, 0, 0, 0, 0],
-                        rolls: [0, 0, 0, 0, 0, 0, 0, 0]
+                        rolls: [0, 0, 0, 0, 0, 0, 0, 0],
+                        labels: ['Rot', 'Gelb', 'Blau', 'Hellblau', 'Grün', 'Orange', 'Pink', 'Schwarz'],
                     },
                     ]     
                },
@@ -78,21 +80,28 @@ export default{
                     plugins:{
                         tooltip: {
                             callbacks: {
-                                label: function (context) {
-                                    return (
-                                        context.dataset.label +
-                                        ": " +
-                                        context.dataset.rolls[context.dataIndex] +
-                                        " Würfe" +
-                                        " (" +
-                                        Math.round(
-                                        context.dataset.data[context.dataIndex] 
-                                        ) +
-                                        "%)"
-                                    );
+                                title: function (tooltipItems) {
+                                    if (tooltipItems[0].datasetIndex == 0) {
+                                        return "Würfel 1";
+                                    }
+                                    else {
+                                        return "Würfel 2";
+                                    }
+                                    
+                                },
+                                label: function (context) {                              
+                                return (
+                                    context.label +
+                                    ': ' +
+                                    context.dataset.rolls[context.dataIndex] +
+                                    '-mal geworfen' +
+                                    ' (' +
+                                    Math.round(context.dataset.data[context.dataIndex]) +
+                                    '%)'
+                                );
                                 },
                             },
-                        },
+                            },
                         legend: {
                             display:false
                         },
@@ -123,8 +132,7 @@ export default{
                         }
                     },
                     animation: {
-                        duration: 1500,
-                        //easing: "linear",                   
+                        duration: 1500,                   
                     }         
                 }
             }
@@ -156,15 +164,21 @@ export default{
             setTableToZero(diceId) {
                 
                 this.myChart.data.datasets[diceId].data = [0, 0, 0, 0, 0, 0, 0, 0];
+                this.myChart.stop();
                 this.myChart.update();
         
             },
-            displayChart(rolls, numberOfRolls , datasetIndex) {
+            displayChart(rolls, numberOfRolls , diceSidesInfo, diceSidesInfoOther, datasetIndex) {
             // Generate data for the chart based on rolls
 
                 this.myChart.options.animation.duration = 0;
                 this.setTableToZero(datasetIndex);
-                this.myChart.options.animation.duration = 1500;
+                this.myChart.options.animation.duration = 1500
+
+                let backgroundColor = ['rgba(213, 94, 0, 0.4)', 'rgba(240, 228, 66, 0.2)', 'rgba(0, 114, 178, 0.3)', 'rgba(86, 180, 233, 0.5)', 
+                        'rgba(0, 158, 115, 0.5)', 'rgba(230, 159, 0, 0.5)', 'rgba(204, 121, 167, 0.5)', 'rgba(0, 0, 0, 0.3)'];
+                let borderColor = ['rgb(213, 94, 0)', 'rgb(240, 228, 66)', 'rgb(0,114,178)', 'rgb(86, 180, 233)', 
+                        'rgb(0, 158, 115)', 'rgb(230, 159, 0)', 'rgb(204, 121, 167)', 'rgb(0, 0, 0)'];
 
                 let sidesCount = 0;
 
@@ -182,24 +196,100 @@ export default{
                 else {sidesCount = dice1.sidesInputsCount;}
 
                 var allData = [
-                {rank: 1, label: 'Rot', value: ((rolls.filter(roll => roll === 'red').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'red').length)},
-                {rank: 2, label: 'Gelb', value: ((rolls.filter(roll => roll === 'yellow').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'yellow').length)},
-                {rank: 3, label: 'Blau', value: ((rolls.filter(roll => roll === 'blue').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'blue').length)},
-                {rank: 4, label: 'Hellblau', value: ((rolls.filter(roll => roll === 'lightblue').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'lightblue').length)},
-                {rank: 5, label: 'Grün', value: ((rolls.filter(roll => roll === 'green').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'green').length)},
-                {rank: 6, label: 'Orange', value: ((rolls.filter(roll => roll === 'orange').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'orange').length)},
-                {rank: 7, label: 'Pink', value: ((rolls.filter(roll => roll === 'pink').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'pink').length)},
-                {rank: 8, label: 'Schwarz', value: ((rolls.filter(roll => roll === 'black').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'black').length)},
+                {rank: 1, label: 'Rot', value: ((rolls.filter(roll => roll === 'red').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'red').length), sides: diceSidesInfo[0].sides},
+                {rank: 2, label: 'Gelb', value: ((rolls.filter(roll => roll === 'yellow').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'yellow').length), sides: diceSidesInfo[1].sides},
+                {rank: 3, label: 'Blau', value: ((rolls.filter(roll => roll === 'blue').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'blue').length), sides: diceSidesInfo[2].sides},
+                {rank: 4, label: 'Hellblau', value: ((rolls.filter(roll => roll === 'lightblue').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'lightblue').length), sides: diceSidesInfo[3].sides},
+                {rank: 5, label: 'Grün', value: ((rolls.filter(roll => roll === 'green').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'green').length), sides: diceSidesInfo[4].sides},
+                {rank: 6, label: 'Orange', value: ((rolls.filter(roll => roll === 'orange').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'orange').length), sides: diceSidesInfo[5].sides},
+                {rank: 7, label: 'Pink', value: ((rolls.filter(roll => roll === 'pink').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'pink').length), sides: diceSidesInfo[6].sides},
+                {rank: 8, label: 'Schwarz', value: ((rolls.filter(roll => roll === 'black').length) / numberOfRolls) * 100, rolls: (rolls.filter(roll => roll === 'black').length), sides: diceSidesInfo[7].sides},
                 ]
 
-                var newData = allData.filter(data => data.rank <= sidesCount).map(data => data.value);
-                var newLabels = allData.filter(data => data.rank <= sidesCount).map(data => data.label);
-                var newRolls = allData.filter(data => data.rank <= sidesCount).map(data => data.rolls);
+                if (diceSidesInfoOther.length != 0) {
+                    var newData = allData.filter(data => (data.rank <= sidesCount & (data.sides > 0 |diceSidesInfoOther[data.rank-1].sides > 0))).map(data => data.value);
+                    var newLabels = allData.filter(data => (data.rank <= sidesCount & (data.sides > 0 |diceSidesInfoOther[data.rank-1].sides > 0))).map(data => data.label);
+                    var newRolls = allData.filter(data => (data.rank <= sidesCount & (data.sides > 0 |diceSidesInfoOther[data.rank-1].sides > 0))).map(data => data.rolls);
+                }
+                else {
+                    var newData = allData.filter(data => (data.rank <= sidesCount & data.sides > 0)).map(data => data.value);
+                    var newLabels = allData.filter(data => (data.rank <= sidesCount & data.sides > 0)).map(data => data.label);
+                    var newRolls = allData.filter(data => (data.rank <= sidesCount & data.sides > 0)).map(data => data.rolls);
+                }
 
+                var newColor = [];
+                var newBorderColor = [];
+
+                for (var i = 0; i <= sidesCount; i++) {
+                    //Farben bei denen einer der beiden Würfel eine Seitenanzahl größer 0 hat müssen vorkommen
+                    if (diceSidesInfoOther.length != 0) {
+                        if (diceSidesInfo[i].sides > 0 | diceSidesInfoOther[i].sides > 0) {
+                            newColor.push(backgroundColor[i]);
+                            newBorderColor.push(borderColor[i]);
+                        }
+                    }
+                    else {
+                        if (diceSidesInfo[i].sides > 0) {
+                            newColor.push(backgroundColor[i]);
+                            newBorderColor.push(borderColor[i]);
+                        }
+                    }
+                }
+
+                if (datasetIndex == 0 && diceSidesInfoOther.length != 0) {
+                    let dice2NewData = [];
+                    let dice2NewRolls = [];
+
+                    let bias = 0;
+
+                    for (let i = 0; i < newLabels.length; i++) {
+                        
+                        if (this.myChart.data.datasets[1].labels.includes(newLabels[i])) {
+                            dice2NewData.push(this.myChart.data.datasets[1].data[i - bias]);
+                            dice2NewRolls.push(this.myChart.data.datasets[1].rolls[i - bias]);
+                        }
+                        else {
+                            dice2NewData.push(0);
+                            dice2NewRolls.push(0);
+                            bias++;
+                        }
+                    }
+                    this.myChart.data.datasets[1].rolls = dice2NewRolls;
+                    this.myChart.data.datasets[1].labels = newLabels;
+                    this.myChart.data.datasets[1].data = dice2NewData;
+                    this.myChart.data.datasets[1].backgroundColor = newColor;
+                    this.myChart.data.datasets[1].borderColor = newBorderColor;
+                }
+                else if (datasetIndex == 1) {
+                    let dice1NewData = [];
+                    let dice1NewRolls = [];
+
+                    let bias = 0;
+
+                    for (let i = 0; i < newLabels.length; i++) {
+                        if (this.myChart.data.datasets[0].labels.indexOf(newLabels[i]) != -1) {
+                            dice1NewData.push(this.myChart.data.datasets[0].data[i - bias]);
+                            dice1NewRolls.push(this.myChart.data.datasets[0].rolls[i - bias]);
+                        }
+                        else {
+                            dice1NewData.push(0);
+                            dice1NewRolls.push(0);
+                            bias++;
+                        }
+                    }
+                    this.myChart.data.datasets[0].rolls = dice1NewRolls;
+                    this.myChart.data.datasets[0].labels = newLabels;
+                    this.myChart.data.datasets[0].data = dice1NewData;
+                    this.myChart.data.datasets[0].backgroundColor = newColor;
+                    this.myChart.data.datasets[0].borderColor = newBorderColor;
+                }
 
                 this.myChart.data.datasets[datasetIndex].rolls = newRolls;
                 this.myChart.data.labels = newLabels;
+                this.myChart.data.datasets[datasetIndex].labels = newLabels;
                 this.myChart.data.datasets[datasetIndex].data = newData;
+                this.myChart.data.datasets[datasetIndex].backgroundColor = newColor;
+                this.myChart.data.datasets[datasetIndex].borderColor = newBorderColor;
 
                 this.updateChart(datasetIndex);  
                     
@@ -212,7 +302,14 @@ export default{
                     return;
                 }
                 dice1.rollDice();
-                this.displayChart(dice1.rolls, dice1.numberOfRolls, 0);
+                
+                if (this.secondDice) {
+                    let dice2 = this.$refs.dice2;
+                    this.displayChart(dice1.rolls, dice1.numberOfRolls, dice1.diceSidesInfo, dice2.diceSidesInfo, 0);
+                }
+                else {
+                    this.displayChart(dice1.rolls, dice1.numberOfRolls, dice1.diceSidesInfo, [], 0);
+                }
             },
 
             rollDice2(){
@@ -223,7 +320,11 @@ export default{
                     return;
                 }
                 dice2.rollDice();
-                this.displayChart(dice2.rolls, dice2.numberOfRolls, 1);
+
+            
+                let dice1 = this.$refs.dice1;
+                this.displayChart(dice2.rolls, dice2.numberOfRolls, dice2.diceSidesInfo, dice1.diceSidesInfo, 1);
+                
             },
             
             changeChart() {
